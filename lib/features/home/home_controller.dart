8aa5d5bca94650c6498398c50deb592498e0f559
Hagar Mahmoud <hagar.mahmoud1011@gmail.com>
2026-07-20 16:43:16 +0300

    some enhancement with provider

---
+++ lib/features/home/home_controller.dart
@@ -0,0 +1,86 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:tasky/models/task_model.dart';
+
+import '../../core/constants/storage_key.dart';
+import '../../core/services/preferences_manager.dart';
+
+class HomeController with ChangeNotifier {
+  List<TaskModel> tasksList = [];
+  String? username;
+  String? motivationQuote;
+  List<TaskModel> tasks = [];
+
+  bool isLoading = false;
+  int totalTask = 0;
+  int totalDoneTasks = 0;
+  double percent = 0;
+  String? userImagePath;
+
+  init (){
+  loadUserDetails();
+  loadTask();
+
+}
+  Future<void> loadTask() async {
+    isLoading = true;
+
+
+    final finalTask = PreferencesManager().getString(StorageKey.tasks);
+
+    if (finalTask != null) {
+      final taskDecode = jsonDecode(finalTask) as List<dynamic>;
+
+
+      tasks = taskDecode
+          .map((element) => TaskModel.fromJson(element))
+          .toList();
+
+       calculatePercent();
+    }
+
+    isLoading = false;
+    notifyListeners();
+  }
+
+  void loadUserDetails() {
+    username = PreferencesManager().getString(StorageKey.username);
+
+    motivationQuote =
+        PreferencesManager().getString(StorageKey.motivationQuote);
+    userImagePath = PreferencesManager().getString(StorageKey.userImage);
+    notifyListeners();
+  }
+
+  void calculatePercent() {
+    totalTask = tasks.length;
+    totalDoneTasks = tasks.where((e) => e.isDone).length;
+    percent = totalTask == 0 ? 0 : totalDoneTasks / totalTask;
+    notifyListeners();
+  }
+
+  Future<void> deleteTask(int? id) async {
+    if (id == null) return;
+
+      tasks.removeWhere((task) => task.id == id);
+      calculatePercent();
+
+    final updatedTask = tasks.map((element) => element.toJson()).toList();
+    await PreferencesManager().setString(StorageKey.tasks, jsonEncode(updatedTask));
+    notifyListeners();
+  }
+
+  Future<void> doneTask(bool? value, int? index) async {
+
+      tasks[index!].isDone = value ?? false;
+      calculatePercent();
+
+
+    final updatedTask = tasks.map((element) => element.toJson()).toList();
+    await PreferencesManager().setString(StorageKey.tasks, jsonEncode(updatedTask));
+      notifyListeners();
+  }
+
+
+}
